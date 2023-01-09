1d9f7fc8c7e48ad5bb3d6bcf493363f2b26fcadf
utkarsh-1mg <utkarsh.srivastava@1mg.com>
2023-01-09 19:27:38 +0530

    change test description for hydration mismatches

--- src/__tests__/loadable-components/intersection-observer_present.test.js
+++ src/__tests__/loadable-components/intersection-observer_present.test.js
@@ -191,7 +191,7 @@ describe("the component loads for the second time", () => {
     });
 });
 
-describe("match output on server and client", () => {
+describe("test hydration mismatches", () => {
     beforeAll(() => {
         jest.dontMock("@loadable/component");
     });
