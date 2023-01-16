0ec5dc3ffc8585c01874bde903329d758125587b
utkarsh-1mg <utkarsh.srivastava@1mg.com>
2023-01-16 14:00:36 +0530

    add support for config file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+const { cosmiconfigSync } = require("react-loadable-visibility");
+
+const explorerSync = cosmiconfigSync("loadableConfig.json");
+
+const searchedFor = explorerSync.search();
+// const loaded = explorerSync.load(pathToConfig);
+
+console.log(searchedFor);
 module.exports = require("./loadable-components");
